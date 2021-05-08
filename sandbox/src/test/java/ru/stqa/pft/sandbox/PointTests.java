ab8128b86ef262ccbf24f677742c5d1ecab4c8d0
natalyaalansi <alansinatalya@gmail.com>
2021-05-08 12:06:19 +0300

    Подключен TestNG и созданы тесты для класса Point (Задание №3)

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistanceCase1() {
+    Point p1 = new Point(3, 7);
+    Point p2 = new Point(-2, -6);
+    Assert.assertEquals(p1.distance(p2), 13.92838827718412);
+  }
+
+  @Test
+  public void testDistanceCase2() {
+    Point p1 = new Point(2.0, 3.0);
+    Point p2 = new Point(4.0, 8.0);
+    Assert.assertEquals(p1.distance(p2), 5.385164807134504);
+  }
+}
