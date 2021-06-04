60e257eb1369a0dd49e21f0135daee4f1c8b56c5
natalyaalansi <alansinatalya@gmail.com>
2021-06-04 19:38:12 +0300

    Задание 16 Попытка 25

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddUserToGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddUserToGroupTests.java
@@ -4,9 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 import ru.stqa.pft.addressbook.model.UserData;
+import ru.stqa.pft.addressbook.model.Users;
 
 public class AddUserToGroupTests extends TestBase {
+
+  private int maxId;
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -15,7 +19,7 @@ public class AddUserToGroupTests extends TestBase {
       app.group().create(new GroupData().withName("test1"));
     }
 
-    if (app.db().users().size() == 0 || app.db().usersWithoutGroup().size() == 0) {
+    if (app.db().users().size() == 0) {
       app.goTo().userPage();
       app.user().create(new UserData().withFirstname("petr").withLastname("petrov").withAddress("spb").withHomePhone("lenina").withEmail("gg@gg.gg"), true);
     }
@@ -23,11 +27,33 @@ public class AddUserToGroupTests extends TestBase {
 
   @Test
   public void testAddUserToGroup() {
-    UserData user = app.db().users().iterator().next();
-    GroupData group = app.db().groups().iterator().next();
-    app.goTo().homePage();
-    app.user().addUserToGroup(user, group);
-    UserData addedUser = app.db().userById(user.getId());
-    Assert.assertTrue(addedUser.getGroups().contains(group));
+    GroupData modifyGroup = app.db().groups().iterator().next();
+    UserData modifyUser = new UserData();
+    Users usersBefore = app.db().users();
+    boolean createUser = false;
+
+    for (UserData user : usersBefore) {
+      if (user.getGroups().size() == 0) {
+        modifyUser = user;
+        app.user().addUserToGroup(modifyUser, modifyGroup);
+        break;
+      } else {
+        createUser = true;
+      }
+    }
+    if (createUser) {
+      app.goTo().userPage();
+      app.user().create(new UserData().withFirstname("petr").withLastname("petrov")
+              .withAddress("spb").withHomePhone("lenina").withEmail("gg@gg.gg"), true);
+      Users getUsersAfterCreation = app.db().users();
+      for (UserData eachUser : getUsersAfterCreation) {
+        if (eachUser.getId() > maxId) {
+          modifyUser = eachUser;
+        }
+        app.user().addUserToGroup(modifyUser, modifyGroup);
+      }
+    }
+    UserData addedUser = app.db().userById(modifyUser.getId());
+    Assert.assertTrue(addedUser.getGroups().contains(modifyGroup));
   }
 }
